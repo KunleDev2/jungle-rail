d84643a3edd8c34218c1c9feb00e509d98cc9e36
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-10-13 09:36:14 -0500

    Implement model spec rails test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip.downcase)
+    user && user.authenticate(password) ? user : nil
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'password and password_confirmation should be created' do
+      @user = User.new(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).to be_valid
+    end
+
+    it 'if password and password_confirmation do not match then it should be invalid' do
+      @user = User.new(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'wrongpassword'
+      )
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation does not match Password")
+    end
+
+    it 'password_confirmation is required' do
+      @user = User.new(
+        email: 'test@test.com',
+        password: 'password'
+      )
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation can not be empty")
+    end
+
+    it 'email is required' do
+      @user = User.new(
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Email can not be empty")
+    end
+
+    it 'email (not case sensitive) should be unique' do
+      @user = User.new(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      @user.save
+
+      @user2 = User.new(
+        email: 'TEST@TEST.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("This email is already taken")
+    end
+
+    it 'password length of 6 characters minimum' do
+      @user = User.new(
+        email: 'test@test.com',
+        password: 'short',
+        password_confirmation: 'short'
+      )
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'if email and password match then it should authenticate' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('test@test.com', 'password')
+      expect(authenticated_user).to eq(user)
+    end
+
+    it 'if email and password do not match, return nil' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('test@test.com', 'wrongpassword')
+      expect(authenticated_user).to be_nil
+    end
+
+    it 'if email has leading/trailing spaces it should authenticate' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      authenticated_user = User.authenticate_with_credentials(' test@test.com ', 'password')
+      expect(authenticated_user).to eq(user)
+    end
+
+    it 'should authenticate regardless of email case' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('TEST@TEST.COM', 'password')
+      expect(authenticated_user).to eq(user)
+    end
+  end
 end
